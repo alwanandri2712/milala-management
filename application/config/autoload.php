d98cd7ab08d36808ea8324336f7bb2ef8c1d77e1
Alwan Putra Andriansyah <alwanputra2712@gmail.com>
2024-02-09 17:50:55 +0700

    library whatsapp

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'access','uuid' ,'notification_list');
+$autoload['libraries'] = array('database', 'session', 'access','uuid' ,'notification_list','whatsapp');
 
 /*
 | -------------------------------------------------------------------
